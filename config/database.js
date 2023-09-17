f789af68674d96f96fb4b07e81c58e0ea27745e1
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2023-09-17 13:50:38 +0300

    modified db config

---
+++ config/database.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('PGHOST', '127.0.0.1'),
+        port: env.int('PGPORT', 5675),
+        database: env('PGDATABASE', 'railway'),
+        user: env('PGUSER', 'postgres'),
+        password: env('PGPASSWORD', 'password'),
+        ssl: env.bool(true),
+      },
+    },
+  });
+
+  // this should fucking work
